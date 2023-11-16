86af85ec96873b2e7079369e53e2da9b7b728d13
Victor Hjelmberg Feddersen <69452489+victorhjelmberg@users.noreply.github.com>
2023-11-16 13:40:37 +0100

    Added users to database. Along with functions: add_user, print_users() and get_user.

---
+++ src/database.h
@@ -0,0 +1,22 @@
+//
+// Created by feddersen on 16/11/2023.
+//
+
+#ifndef P2P_FOOD_DONATION_DATABASE_H
+#define P2P_FOOD_DONATION_DATABASE_H
+
+#endif //P2P_FOOD_DONATION_DATABASE_H
+
+enum user_role{role_admin, role_user};
+
+struct user{
+    char username[30];
+    char password[30];
+    char phone_number[15];
+    int points;
+    enum user_role role;
+};
+
+void add_user(char* username, char* password, char* phone_number, int points, enum user_role role);
+struct user get_user(char* username);
+void print_users();
---
+++ src/main.c
@@ -0,0 +1,9 @@
+//
+// Created by feddersen on 14/11/2023.
+//
+#include "database.h"
+
+int main(void){
+    //add_user("Victor", "Password", "88888888", 100, role_user);
+    //print_users();
+}
