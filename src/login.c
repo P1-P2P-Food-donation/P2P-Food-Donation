6c484e09570648e43f78daa14b66f32dd1c8c16b
EmilVorre <ab74as@student.aau.dk>
2023-11-18 20:03:24 +0100

    updated .gitignore

---
+++ src/login.c
@@ -0,0 +1,5 @@
+//
+// Created by Emil on 16-11-2023.
+//
+
+#include "login.h"
---
+++ src/login.h
@@ -0,0 +1,8 @@
+//
+// Created by Emil on 16-11-2023.
+//
+
+#ifndef MERGESORT_LOGIN_H
+#define MERGESORT_LOGIN_H
+
+#endif //MERGESORT_LOGIN_H
