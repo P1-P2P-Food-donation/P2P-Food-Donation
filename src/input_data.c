a2cdfb4c7566a2f4dbf91a9ba559ca7a0dede9a6
EmilVorre <ab74as@student.aau.dk>
2023-12-11 04:17:53 +0100

    added all functions to input data
    void read_data();
    FILE* openfile(const char *filename);
    enum user_role role_from_string(const char *role_str);
    void read_and_add_users();
    enum item_category category_from_string(const char *category_str);
    void read_and_add_items();

--- src/input_data.c
+++ src/input_data.c
@@ -3,35 +3,85 @@
 //
 #include <stdlib.h>
 #include <stdio.h>
-#include "database.h"
 #include "string.h"
 #include "input_data.h"
-void input_data(){
-    input_users();
-    input_items();
+#include "database.h"
+
+void read_data(){
+    read_and_add_users();
+    read_and_add_items();
 }
-void input_items(){
-    char username[30];
-    char title[30];
-    char description[100];
-    char location[100];
-    char category[30];
-    int quantity;
-    openfile("items.txt");
-    while(fscanf(file, "%s,%s,%s,%s,%s,%d", username, title, description, location, &category, quantity) != EOF){
-        add_item(username, title, description, location, category, quantity);
+
+FILE* openfile(const char *filename){
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
     }
-    closefile();
+    return file;
 }
-void input_users(){
-    char username[30];
-    char password[30];
-    char phone_number[15];
-    int points;
-    char user_role[30];
-    openfile("users.txt");
-    while(fscanf(file, "%s %s %s %d %d", username, password, phone_number, &points, &role) != EOF){
+
+enum user_role role_from_string(const char *role_str) {
+    if (strcmp(role_str, "role_user") == 0) {
+        return role_user;
+    } else if (strcmp(role_str, "role_admin") == 0) {
+        return role_admin;
+    }
+    // Add more conditions as needed
+    return role_user; // Default to ROLE_USER if unknown
+}
+
+void read_and_add_users() {
+    FILE* file = openfile("users");
+
+    char line[256]; // Adjust the buffer size as needed
+    while (fgets(line, sizeof(line), file) != NULL) {
+        // Tokenize the line
+        char *username = strtok(line, ",");
+        char *password = strtok(NULL, ",");
+        char *phone_number = strtok(NULL, ",");
+        int points = atoi(strtok(NULL, ","));
+        char *role_str = strtok(NULL, ",");
+
+        // Convert role string to enum value
+        enum user_role role = role_from_string(role_str);
+
+        // Call the add_user function
         add_user(username, password, phone_number, points, role);
     }
-    closefile();
+
+    fclose(file);
+}
+
+enum item_category category_from_string(const char *category_str) {
+    if (strcmp(category_str, "fruit") == 0) {
+        return fruit;
+    } else if (strcmp(category_str, "vegetable") == 0) {
+        return vegetable;
+    }
+    // Add more conditions as needed
+    return fruit; // Default to FRUIT if unknown
+}
+
+void read_and_add_items() {
+    FILE* file = openfile("items");
+
+    char line[256]; // Adjust the buffer size as needed
+    while (fgets(line, sizeof(line), file) != NULL) {
+        // Tokenize the line
+        char *username = strtok(line, ",");
+        char *title = strtok(NULL, ",");
+        char *description = strtok(NULL, ",");
+        char *location = strtok(NULL, ",");
+        char *category_str = strtok(NULL, ",");
+        int quantity = atoi(strtok(NULL, ","));
+
+        // Convert category string to enum value
+        enum item_category category = category_from_string(category_str);
+
+        // Call the add_item function
+        add_item(username, title, description, location, category, quantity);
+    }
+
+    fclose(file);
 }
--- src/input_data.h
+++ src/input_data.h
@@ -3,8 +3,12 @@
 //
 #ifndef P2P_FOOD_DONATION_INPUT_DATA_H
 #define P2P_FOOD_DONATION_INPUT_DATA_H
-#include "database.h"
-void input_data();
-void input_items();
-void input_users();
+
+void read_data();
+FILE* openfile(const char *filename);
+enum user_role role_from_string(const char *role_str);
+void read_and_add_users();
+enum item_category category_from_string(const char *category_str);
+void read_and_add_items();
+
 #endif //P2P_FOOD_DONATION_INPUT_DATA_H
